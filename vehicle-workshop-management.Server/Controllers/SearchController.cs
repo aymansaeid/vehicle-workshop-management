2a9fe22139291a2904e86626127aa3a290d064ee
Ayman Saeid <aemon.saeid@gmail.com>
2025-08-12 06:59:18 +0300

    SearchController created

---
+++ vehicle-workshop-management.Server/Controllers/SearchController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using vehicle_workshop_management.Server.Models;
+
+namespace vehicle_workshop_management.Server.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class SearchController : ControllerBase
+    {
+        private readonly DBCONTEXT _context;
+
+        public SearchController(DBCONTEXT context)
+        {
+            _context = context;
+        }
+
+        
+    }
+}
